ee8aa5a18eaff527a51ce963a409340a886bfdce
giancoppola <44214851+giancoppola@users.noreply.github.com>
2025-03-09 20:01:50 +0000

    jest testing setup with global before and after functions

---
+++ automated_tests/h1Tags.test.ts
@@ -0,0 +1,20 @@
+import {driver, websiteToTest} from "../automated_test_modules/_start-parameters";
+import {By, WebElement} from "selenium-webdriver";
+
+// let siteLinks = await driver.findElements(By.css("a"));
+// let internalLinks = [...pagesToTest];
+// internalLinks = internalLinks.concat(await GetInternalLinks(siteLinks));
+// console.log(internalLinks);
+// let title = await driver.getTitle();
+// console.log(title);
+
+describe("H1 Tags Test", ()=>{
+    it("All pages have at least 1 H1 tag", async () => {
+
+        await driver.get(websiteToTest);
+        let h1Tags: WebElement[] = await driver.findElements(By.css('h1'));
+        expect(h1Tags).not.toBe(null);
+        expect(h1Tags.length).toBeGreaterThan(0);
+    })
+})
+
