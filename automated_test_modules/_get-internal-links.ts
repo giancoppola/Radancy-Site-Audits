e2bc3e0eefb91bbd4bc9839b4fdba9151324e632
Gian <giancoppola@hotmail.co.uk>
2025-03-14 16:18:30 +0000

    pts link setup

--- automated_test_modules/_get-internal-links.ts
+++ automated_test_modules/_get-internal-links.ts
@@ -1,19 +1,23 @@
 import {By, ThenableWebDriver, until, WebElement} from "selenium-webdriver";
 import {pagesToTest, websiteToTest} from "../automated_test_setup/_test-parameters";
 
-
 export default async function GetInternalLinks(driver: ThenableWebDriver): Promise<string[]> {
-    let linksToTest: string[] = [websiteToTest];
-    for(let page of pagesToTest) {
-        linksToTest.push(websiteToTest + page);
+    let homepage = (" " + websiteToTest).slice(1);
+    if (homepage.endsWith("/")){
+        homepage = homepage.slice(0, -1);
     }
-    await driver.get(websiteToTest);
+    let linksToTest: string[] = [homepage];
+    for(let page of pagesToTest) {
+        linksToTest.push(homepage + page);
+    }
+    await driver.get(homepage);
     await driver.wait(until.elementsLocated(By.css("a")));
     let aTags = await driver.findElements(By.css("a"));
     for (let a of aTags) {
         let link = await a.getAttribute("href");
-        let internalLink = link.startsWith("/") || link.startsWith(websiteToTest) || link.startsWith(websiteToTest);
+        let internalLink = link.startsWith("/") || link.startsWith(homepage) || link.startsWith(homepage);
         if (link && internalLink && !linksToTest.includes(link)) {
+            // selenium will figure out absolute URLs from the relative URLs
             linksToTest.push(link);
         }
     }
--- automated_test_setup/_test-parameters.ts
+++ automated_test_setup/_test-parameters.ts
@@ -2,9 +2,8 @@
 // Careers site you want to test, this should be the homepage of the site
 export const websiteToTest: string = "https://careers.questdiagnostics.com/";
 
-// Array of core pages to test, the automated tests will also
-// add internal pages linked on the site, but you can add here
-// to be certain they will be tested
+// Array of relative links of pages to test
+// The pre-test setup will also add internal pages linked on the homepage
 export const pagesToTest: string[] = [
     "/search-jobs",
     "/sitemap",
